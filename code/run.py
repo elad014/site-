4eefad08eb42f155f4d4390deba7895d06f8442e
elad <elad.glx@gmail.com>
2025-03-02 18:27:22 +0200

    ver 1.0

---
+++ code/run.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("main.html")  # Serve main.html from templates/html/
+
+@app.route('/goToManagerPage')
+def goToManagerPage():
+    return render_template("manager.html")  # Serve main.html from templates/
+
+@app.route('/git_update', methods=['POST'])
+def git_update():
+    print("Clicked")  # This prints to the server terminal
+    return jsonify({"message": "Button clicked!"})  # Respond to client
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=106, debug=False)
