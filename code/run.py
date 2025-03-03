7e775845bdcfe86037daa8529cf654ca0925d252
elad <elad.glx@gmail.com>
2025-03-03 22:18:04 +0200

    ver 1.9.7

--- code/run.py
+++ code/run.py
@@ -16,7 +16,7 @@ def goToManagerPage():
 @app.route('/git_update', methods=['POST'])
 def git_update():
     print("[INFO] Update files from git w8 4 finish!!")
-    os.system("cd /home/ubuntu/Desktop/site-/code && git reset --hard HEAD  && git pull https://github.com/elad014/site-.git master --progress")
+    os.system("cd /home/ubuntu/Desktop/site-/ && git reset --hard HEAD  && git pull https://github.com/elad014/site-.git master --progress")
     return {'message': 'Git update successful'}, 200
 
 def read_version():
